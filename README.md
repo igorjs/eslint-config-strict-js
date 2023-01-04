9abdbb9711dd334378c9353691fc81dce5091e11
Igor J. Santos <igor@igorjs.io>
2023-01-05 02:29:45 +1100

    add more rules and configs

--- README.md
+++ README.md
@@ -1,20 +1,77 @@
-# eslint-strict-js
+<div align="center">
+  <center>
+    <img width="148" height="148" alt="Strict ESLint Config logo" src="https://raw.githubusercontent.com/igorjs/eslint-config-strict-js/main/docs/eslint.png" />
+  </center>
+</div>
+<div align="center">
+  <center>
+    <h1 align="center"><i></i>A Strict, Awesome ESLint Config<i></i></h1>
+  </center>
+</div>
 
 Base eslint config for latest ecma script standards with strict rules
 
 ## Usage
+To make use of this configuration, you need to first install the configuration in your Node.js project:
 
 ```bash
-npm i -D @igorjs/eslint-config-strict-js
+npm install --save-dev eslint-config-strict-js
+```
+
+After that, you need to include a reference to the package in your package.json:
+
+```json
+{
+  "name": "mypackage",
+  "version": "1.0.0",
+  ...
+  "eslintConfig": {
+    "extends": "@igorjs/eslint-config-strict-js"
+  }
+}
+```
+
+You can override the settings that are provided by the plugin by including additional rules in your package.json. For instance, if you wanted to turn off a rule named max-len, your package.json would look something like this:
+
+```json
+{
+  "name": "mypackage",
+  "version": "1.0.0",
+  ...
+  "eslintConfig": {
+    "extends": "@igorjs/eslint-config-strict-js",
+    "rules": {
+        "max-len": "off"
+    }
+  }
+}
 ```
 
 In `.eslintrc`:
 
-```
-root: true
-
-extends:
-  - '@igorjs/eslint-config-strict-js'
+```json
+{
+  "root": true,
+  "extends": ["@igorjs/eslint-config-strict-js"],
+  "rules": {
+      "max-len": "off"
+  }
+}
 ```
 
 **NOTE:** you may need to restart your editor for config to take effect
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome! Feel free to check the [issues] page. If you would like to contribute, please take a look at the [contributing] guide.
+
+
+## Plugins
+
+
+## License
+This project is [MIT][license] licensed.
+
+[issues]: https://github.com/igorjs/eslint-config-strict-js/issues
+[license]: ./LICENSE
+[contributing]: ./docs/CONTRIBUTING.md
--- index.js
+++ index.js
@@ -1,64 +1,137 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:import/recommended',
-    'plugin:sonarjs/recommended',
-    'plugin:security/recommended',
-    'plugin:jest/recommended',
-    'plugin:jest-dom/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:security/recommended",
+    "plugin:prettier/recommended",
   ],
+
+  plugins: [
+    "@babel",
+    "import",
+    "sonarjs",
+    "security",
+    "prettier",
+    "jest-dom",
+    "jest",
+  ],
+
+  parser: "@babel/eslint-parser",
 
   parserOptions: {
     ecmaFeatures: {
+      impliedStrict: true,
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
 
   env: {
     browser: true,
     node: true,
-    jest: true,
     es6: true,
   },
 
+  globals: {
+    Set: true,
+    Map: true,
+    WeakSet: true,
+    WeakMap: true,
+    Symbol: true,
+    Promise: true,
+    Reflect: true,
+    Uint8ClampedArray: true,
+    module: true,
+  },
+
   settings: {
-    react: {
-      version: 'detect',
+    "import/extensions": [
+      ".mjs",
+      ".cjs",
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".d.ts",
+      ".json",
+      ".wasm",
+    ],
+    "import/cache": {
+      lifetime: Infinity,
     },
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+    "import/resolver": {
+      node: {},
       typescript: {},
     },
   },
 
   rules: {
-    'linebreak-style': ['error', 'unix'],
-    'no-unused-vars': [
-      'warn',
+    "linebreak-style": ["error", "unix"],
+    "no-unused-vars": [
+      "warn",
       {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
       },
     ],
-    'no-nested-ternary': 'error',
-    'no-unneeded-ternary': 'error',
-    'max-lines-per-function': ['error', { max: 50, skipBlankLines: true, skipComments: true }],
-    'max-nested-callbacks': ['error', 3],
-    'max-statements': ['warn', 10],
-    'max-params': ['warn', 4],
-    'max-lines': ['warn', { max: 300, skipBlankLines: true, skipComments: true }],
-    'max-depth': ['warn', 4],
-    complexity: ['warn', 5],
-    indent: ['error', 2],
-    semi: ['error', 'always'],
+    "no-nested-ternary": "error",
+    "no-unneeded-ternary": "error",
+    "max-lines-per-function": [
+      "error",
+      { max: 50, skipBlankLines: true, skipComments: true },
+    ],
+    "max-nested-callbacks": ["error", 3],
+    "max-statements": ["warn", 10],
+    "max-params": ["warn", 4],
+    "max-lines": [
+      "warn",
+      { max: 200, skipBlankLines: true, skipComments: true },
+    ],
+    "max-depth": ["warn", 4],
+    complexity: ["warn", 5],
+    eqeqeq: ["warn", "allow-null"],
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
   },
+
+  overrides: [
+    {
+      files: ["**/*.(test|spec).(js|jsx|ts|tsx)"],
+      env: { jest: true },
+      extends: ["plugin:jest-dom/recommended", "plugin:jest/recommended"],
+      globals: {
+        MouseEvent: true,
+      },
+      plugins: ["jest"],
+      rules: {
+        "jest/prefer-expect-assertions": "warn",
+        "no-magic-numbers": "off",
+      },
+    },
+    {
+      files: ["scripts/*.js"],
+      env: { node: true },
+      globals: {
+        require: true,
+        module: true,
+      },
+      rules: {
+        "no-magic-numbers": "off",
+      },
+    },
+    {
+      files: ["jest.config.js"],
+      env: { node: true },
+      globals: {
+        require: true,
+        module: true,
+      },
+      rules: {
+        "no-magic-numbers": "off",
+      },
+    },
+  ],
 };
